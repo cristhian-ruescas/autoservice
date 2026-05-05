0291747eca597d030466c12fa7c008af53ee1a18
cristhian-ruescas <cristhian.ruescas83@gmail.com>
2026-05-04 23:43:53 -0300

    adicionado novos testes

--- src/test/java/com/autoservice/application/ordemservico/diagnostico/IniciarDiagnosticoUseCaseTest.java
+++ src/test/java/com/autoservice/application/ordemservico/diagnostico/IniciarDiagnosticoUseCaseTest.java
@@ -1,6 +1,7 @@
 package com.autoservice.application.ordemservico.diagnostico;
 
 import com.autoservice.domain.events.DomainEventPublisher;
+import com.autoservice.domain.exceptions.DomainException;
 import com.autoservice.domain.ordemservico.OrdemServico;
 import com.autoservice.domain.ordemservico.OrdemServicoGateway;
 import com.autoservice.domain.ordemservico.enums.OrdemServicoStatus;
