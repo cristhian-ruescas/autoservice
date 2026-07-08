eb10bf066aca795f05c7e9cd25b3c29ffd42c8bc
cristhian-ruescas <cristhian.ruescas83@gmail.com>
2026-07-08 01:15:13 -0300

    corrige testes

--- src/main/java/com/autoservice/infrastructure/config/TransactionalUseCaseAspect.java
+++ src/main/java/com/autoservice/infrastructure/config/TransactionalUseCaseAspect.java
@@ -4,15 +4,29 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Aspect
 @Component
 public class TransactionalUseCaseAspect {
 
+    private final TransactionTemplate transactionTemplate;
+
+    public TransactionalUseCaseAspect(final PlatformTransactionManager transactionManager) {
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
+    }
+
     @Around("execution(* com.autoservice.application..*UseCase.execute(..))")
-    @Transactional
-    public Object transactionalUseCase(final ProceedingJoinPoint joinPoint) throws Throwable {
-        return joinPoint.proceed();
+    public Object transactionalUseCase(final ProceedingJoinPoint joinPoint) {
+        return this.transactionTemplate.execute(status -> {
+            try {
+                return joinPoint.proceed();
+            } catch (final RuntimeException | Error error) {
+                throw error;
+            } catch (final Throwable throwable) {
+                throw new IllegalStateException(throwable);
+            }
+        });
     }
 }
